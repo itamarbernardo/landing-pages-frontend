1f6e0a0ccbaa26f554e9fe00d14942f94475f51a
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-09-21 20:20:15 -0300

    Criado um template Base, realizado os testes e feito algumas correcoes

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,15 +1,10 @@
 import { Heading } from '../../components/Heading';
+import { Base } from '../Base';
+import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
 
 function Home() {
-  return (
-    <div>
-      <Styled.Wrapper>
-        <Heading>O texto que eu quero</Heading>
-        <h1>Hello</h1>
-      </Styled.Wrapper>
-    </div>
-  );
+  return <Base {...mockBase} />;
 }
 
 export default Home;
