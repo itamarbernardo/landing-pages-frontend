b8187f4123f175ed0b0e98a383566fd37ed81d89
Itamar Bernardo <itamarbernardo2013@gmail.com>
2023-09-22 23:55:12 -0300

    Adicionando componente de Loading com animacao

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -3,6 +3,7 @@ import { mapData } from '../../api/map-data';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -32,7 +33,7 @@ function Home() {
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
